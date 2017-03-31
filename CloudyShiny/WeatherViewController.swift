88c56f6cc43f9d2b1533cd11d796697fe44c41f1
kalyandechiraju <kalyandechiraju@outlook.com>
2017-03-31 09:33:26 +0530

    Setup Storyboard

---
+++ CloudyShiny/WeatherViewController.swift
@@ -0,0 +1,44 @@
+//
+//  WeatherViewController.swift
+//  CloudyShiny
+//
+//  Created by Kalyan Dechiraju on 30/03/17.
+//  Copyright © 2017 Codelight Studios. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class WeatherViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let forecastCellIdentifier = "ForecastCell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: forecastCellIdentifier, for: indexPath)
+        return cell
+    }
+
+}
+
